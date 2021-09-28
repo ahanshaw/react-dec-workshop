97c8b78849a1dadb40f14a484f6ef0853e9ced9c
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-09-28 09:06:31 -0500

    add nav style tweaks

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,6 +6,7 @@ const Nav = () => {
 	return (
 		<nav className="address-book__nav">
 			<ul>
+				<li><NavLink to={`/add`} className="btn btn--add" activeClassName="selected">+</NavLink></li>
 				<li><NavLink to={`/a`} className="btn" activeClassName="selected">A</NavLink></li>
 				<li><NavLink to={`/b`} className="btn" activeClassName="selected">B</NavLink></li>
 				<li><NavLink to={`/c`} className="btn" activeClassName="selected">C</NavLink></li>
@@ -18,7 +19,7 @@ const Nav = () => {
 				<li><NavLink to={`/j`} className="btn" activeClassName="selected">J</NavLink></li>
 				<li><NavLink to={`/k`} className="btn" activeClassName="selected">K</NavLink></li>
 				<li><NavLink to={`/l`} className="btn" activeClassName="selected">L</NavLink></li>
-				<li><NavLink to={`/m`} className="btn" activeClassName="selected">N</NavLink></li>
+				<li><NavLink to={`/m`} className="btn" activeClassName="selected">M</NavLink></li>
 				<li><NavLink to={`/n`} className="btn" activeClassName="selected">N</NavLink></li>
 				<li><NavLink to={`/o`} className="btn" activeClassName="selected">O</NavLink></li>
 				<li><NavLink to={`/p`} className="btn" activeClassName="selected">P</NavLink></li>
@@ -32,7 +33,6 @@ const Nav = () => {
 				<li><NavLink to={`/x`} className="btn" activeClassName="selected">X</NavLink></li>
 				<li><NavLink to={`/y`} className="btn" activeClassName="selected">Y</NavLink></li>
 				<li><NavLink to={`/z`} className="btn" activeClassName="selected">Z</NavLink></li>
-				<li><NavLink to={`/add`} className="btn btn--add" activeClassName="selected">+</NavLink></li>
 			</ul>
 		</nav>
 	)
